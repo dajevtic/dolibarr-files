7a3d7e52b5fc7c99decfcc4fb761de4dc736d4f5
milospetkovic <milos.petkovic@livemediagroup.de>
2019-07-19 14:58:13 +0200

    removed unused js functions

--- js/elbmultiupload.js
+++ js/elbmultiupload.js
@@ -1,15 +1,4 @@
 jQuery(document).ready(function() {
-
-    // url anchor
-    var anchor = window.location.hash;
-    var anchor_arr = anchor.split('-');
-    var anchor_nr = anchor_arr[1];
-
-    // add attribute autocomplete="off" on every input
-    $('input[type="text"]').each(function(ind,el) {
-        $(el).prop('autocomplete','off');
-    });
-
     // wrap update file elements with form tag ()
     $('input[name="update_file"]').closest('table').wrap('<form></form>');
 });
@@ -38,7 +27,7 @@ function toggleSubtable(a, ajax) {
                 }
             };
             $.ajax({
-                url: '/elb/ajax/ajax.php',
+                url: '/elbmultiupload/ajax/ajax.php',
                 data:ajaxdata,
                 dataType: "html",
                 type: "POST",
@@ -55,7 +44,6 @@ function toggleSubtable(a, ajax) {
     $(a).find(".ui-icon").removeClass("ui-icon-triangle-1-e");
     checkExpanded(a);
 }
-
 
 function elb_ajax_dialog(url, title, width, options) {
     if(width==undefined) width=800;
@@ -79,28 +67,12 @@ function elb_ajax_dialog(url, title, width, options) {
                 $( "#elb-ajax-dlg" ).dialog( "option", "position", { my: "center", at: "center", of: window } );
                 $("#elb-ajax-dlg-body .classfortooltip").tipTip({maxWidth: "250px", edgeOffset: 10, delay: 50, fadeIn: 50, fadeOut: 50});
             });
-
-
         }
     };
     if(options) {
         $.extend(dlg_options, options);
     }
     $("#elb-ajax-dlg").dialog(dlg_options);
-}
-
-function elb_ajax_submit(el) {
-    var form=$(el).closest('form');
-    var url=form.attr('action');
-    url+='?';
-    url+=form.serialize();
-    url+='&'+$(el).attr('name')+'='+$(el).val();
-    $("#elb-ajax-dlg-body").html('');
-    $("#elb-ajax-dlg-loading").show();
-    $.get(url,function(data){
-        $("#elb-ajax-dlg-loading").hide();
-        $("#elb-ajax-dlg-body").html(data);
-    });
 }
 
 function elb_ajax_action(url,hook,action,params,form) {
@@ -125,91 +97,4 @@ function elb_ajax_action(url,hook,action,params,form) {
             }
         }
     });
-}
-
-function elb_ajax_dialog_response(url,hook,action,getparams,form) {
-    var ajaxdata = {
-        url: url,
-        hook: hook,
-        action: action,
-        params: {
-            params:getparams
-        }
-    };
-    $.ajax({
-        url: '../../ajax/ajax.php',
-        data:ajaxdata,
-        dataType: "html",
-        type: "POST",
-        success:function(data) {
-            $("#dialog-confirm").remove();
-            $("body").append(data);
-        }
-    });
-}
-
-function closedialog() {
-    $(".ui-dialog").remove();
-}
-
-function select_checkboxes(el, chk_class) {
-    var checked = $(el).prop('checked');
-    $('.'+chk_class).each(function(){
-        $(this).prop('checked',checked);
-    });
-}
-
-function trigger_checkboxes(el, chk_class) {
-    var checked = $(el).prop('checked');
-    $('.'+chk_class).each(function(){
-        $(this).trigger('click');
-    });
-}
-
-function elb_multi_select(sel, submit_on_select, height) {
-    var submit_on_select = (typeof submit_on_select === 'undefined') ? true : submit_on_select;
-    var height = (typeof height === 'undefined') ? 245 : height;
-    $(sel).multiselect({
-        height: height,
-        selectedList: 2,
-        close: function() {
-            if(submit_on_select) {
-                $(sel).closest("form").submit();
-            }
-        }
-    });
-    $(sel).next().width($(sel).width());
-}
-
-function elb_multiselect_translations() {
-    // Multiselect translations
-    $(".ui-multiselect-all").each( function (index) {
-        $(this).children('span').eq(1).text(translations.CheckAll);
-    });
-    $(".ui-multiselect-none").each( function (index) {
-        $(this).children('span').eq(1).text(translations.UncheckAll);
-    });
-    $("button.ui-multiselect.ui-widget").each(function(index) {
-        var getpredefvalue = $(this).children('span').eq(1).text();
-        if (getpredefvalue.indexOf(" selected") >= 0) {
-            var exploded = getpredefvalue.split(" ");
-            var secondpart = translations.Selected;
-            var newvalue = exploded[0] + " " + secondpart;
-            $(this).children('span').eq(1).text(newvalue);
-        } else if (getpredefvalue.indexOf(" options") >= 0) {
-            var newvalue = translations.SelectOptions;
-            $(this).children('span').eq(1).text(newvalue);
-        }
-    });
-}
-
-function form_submit(el, action) {
-    var form = $(el).closest('form');
-    var action_field = form.find("[name=action]");
-    if(action_field.length == 0) {
-        action_field = $('<input type="hidden" name="action" value=""/>');
-        form.append(action_field);
-    }
-    action_field.attr("value",action);
-    form.submit();
 }
